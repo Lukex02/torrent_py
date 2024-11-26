bcfb22528481312f4392e0dc05a975c4e0e933a9
Lukex02 <minh.huynhba2004@hcmut.edu.vn>
2024-11-26 22:52:53 +0700

    dọn rác

--- handshake.py
+++ handshake.py
@@ -14,14 +14,7 @@ def handshake(sock, info_hash, peer_id):
     # Nhận phản hồi handshake
     response = sock.recv(100)
     print("Received handshake response!")
-    # print("Received handshake response:", response)
     
-    # print(response[0:68])
-    # print(response[68:74])
-    # print(response[74:79])
-    # print(parse.parse_handshake_response(response[0:68]))
-    # print(parse.parse_bitfield(response[68:74]))
-    # print(parse.parse_unchoke(response[74:79]))
     return (parse.parse_handshake_response(response[0:68]),
             parse.parse_bitfield(response[68:74]),
             parse.parse_unchoke(response[74:79]))

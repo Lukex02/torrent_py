c27dcfcb1c94189755c31bf7af205dbcc0924a2a
Lukex02 <minh.huynhba2004@hcmut.edu.vn>
2024-11-26 13:52:49 +0700

    Update makeTorrent.py

--- makeTorrent.py
+++ makeTorrent.py
@@ -3,7 +3,8 @@ from torrentool.api import Torrent
 def create_torrent(input_path, tracker_url, output_file):
     torrent = Torrent.create_from(input_path)
     torrent.announce_urls = tracker_url
-    # torrent.meta['info']['piece length'] = 16*1024
+    
+    
     if output_file[len(output_file)-8:] != ".torrent":
         correct_output = output_file + ".torrent"
         torrent.to_file(correct_output)

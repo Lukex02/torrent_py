7e4b647994c3cf18e8c8297d02575edbbafe874a
Lukex02 <minh.huynhba2004@hcmut.edu.vn>
2024-11-30 20:40:44 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 from cx_Freeze import setup, Executable
 
+application_exe = Executable(
+    script="main.py",
+    target_name="MMT_Torrent_Py.exe"
+)
+
+tracker_exe = Executable(
+    script="tracker.py",
+    target_name="MMT_Tracker.exe"
+)
+
+
 setup (
     name="Torrent Python",
-    version="4.2",
-    description="",
-    executables=[Executable("main.py",
-                 target_name="MMT_Torrent_Py")
-                 ],
+    version="5.0",
+    description="Torrent application using python with private tracker",
+    executables=[application_exe, tracker_exe],
 )
